101a2e6428ae4f4f369721652cbd905491efcf80
saman azadi <samanazadi1996@gmail.com>
2024-02-25 04:07:22 +0330

    Add UploadFile Api

--- TestTemplate/TestTemplate.WebApi/Controllers/v1/FileController.cs
+++ TestTemplate/TestTemplate.WebApi/Controllers/v1/FileController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
 using System.Net.Mime;
 using System.Threading.Tasks;
 using TestTemplate.Application.Interfaces;
+using TestTemplate.Application.Wrappers;
 
 namespace TestTemplate.WebApi.Controllers.v1
 {
@@ -14,6 +17,20 @@ namespace TestTemplate.WebApi.Controllers.v1
             var bytes = await fileManagerService.Download(name);
 
             return File(bytes, MediaTypeNames.Application.Octet, name);
+        }
+
+        [HttpPost]
+        public async Task<BaseResult<string>> UploadFile(string name, IFormFile file)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+
+                await fileManagerService.Create(name, memoryStream.ToArray());
+                await fileManagerService.SaveChangesAsync();
+
+                return new(name);
+            }
         }
     }
 }
