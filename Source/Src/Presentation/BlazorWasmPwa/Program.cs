f0d2edd6e4fd79e2ab39f5c7d91f597675368a9c
nammadhu@KiSS <madhusudhan.v@live.in>
2024-05-04 11:35:12 +0530

    showing user logged in details under /user page

--- Source/Src/Presentation/BlazorWasmPwa/Program.cs
+++ Source/Src/Presentation/BlazorWasmPwa/Program.cs
@@ -12,7 +12,7 @@ namespace BlazorWasmPwa
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            
             builder.Services.AddOidcAuthentication(options =>
             {
                 //https://learn.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/standalone-with-authentication-library?view=aspnetcore-8.0&tabs=visual-studio
