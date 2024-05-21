439cbbbaed3a90a65bab8b7442d094284df4ce7d
saman azadi <samanazadi1996@gmail.com>
2024-05-21 20:17:12 +0330

    v 8.1.0

--- Source/Src/Core/CleanArchitecture.Application/Wrappers/PagedResponse.cs
+++ Source/Src/Core/CleanArchitecture.Application/Wrappers/PagedResponse.cs
@@ -5,16 +5,13 @@ namespace CleanArchitecture.Application.Wrappers
 {
     public class PagedResponse<T> : BaseResult<List<T>>
     {
-        public int PageNumber { get; }
-        public int PageSize { get; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
         public int TotalPages { get; set; }
         public int TotalItems { get; set; }
         public bool HasPreviousPage => PageNumber > 1;
         public bool HasNextPage => PageNumber < TotalPages;
-        public PagedResponse()
-        {
 
-        }
         public PagedResponse(Error error) : base(error)
         {
         }
