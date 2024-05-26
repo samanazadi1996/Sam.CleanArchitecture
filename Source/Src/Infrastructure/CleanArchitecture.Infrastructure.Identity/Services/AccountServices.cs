934b64c8adbfb60131c5f7c5f3a494694c1f0b2b
saman azadi <samanazadi1996@gmail.com>
2024-05-27 01:12:52 +0330

    Refactor GenerateRandomString method

--- Source/Src/Infrastructure/CleanArchitecture.Infrastructure.Identity/Services/AccountServices.cs
+++ Source/Src/Infrastructure/CleanArchitecture.Infrastructure.Identity/Services/AccountServices.cs
@@ -120,16 +120,9 @@ namespace CleanArchitecture.Infrastructure.Identity.Services
             string GenerateRandomString(int length)
             {
                 const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-                var random = new Random();
-                var result = new StringBuilder(length);
-
-                for (int i = 0; i < length; i++)
-                {
-                    int index = random.Next(chars.Length);
-                    result.Append(chars[index]);
-                }
-
-                return result.ToString();
+                Random random = new Random();
+                return new string(Enumerable.Repeat(chars, length)
+                        .Select(s => s[random.Next(s.Length)]).ToArray());
             }
         }
         private async Task<JwtSecurityToken> GenerateJwtToken(ApplicationUser user)
