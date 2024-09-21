28de85223debf5d78e3db7755a0df3a393dd6b61
saman azadi <samanazadi1996@gmail.com>
2024-09-21 16:58:33 +0330

    'AuditableBaseEntity.cs' was modified and 'BaseEntity.cs' was modified
    Modified : Src/Core/CleanArchitecture.Domain/Common/AuditableBaseEntity.cs
    Modified : Src/Core/CleanArchitecture.Domain/Common/BaseEntity.cs

--- Source/Src/Core/CleanArchitecture.Domain/Common/AuditableBaseEntity.cs
+++ Source/Src/Core/CleanArchitecture.Domain/Common/AuditableBaseEntity.cs
@@ -2,11 +2,15 @@
 
 namespace CleanArchitecture.Domain.Common
 {
-    public abstract class AuditableBaseEntity : BaseEntity
+    public abstract class AuditableBaseEntity<TKey> : BaseEntity<TKey>
     {
         public Guid CreatedBy { get; set; }
         public DateTime Created { get; set; }
         public Guid? LastModifiedBy { get; set; }
         public DateTime? LastModified { get; set; }
+    }
+
+    public abstract class AuditableBaseEntity : AuditableBaseEntity<long>
+    {
     }
 }
