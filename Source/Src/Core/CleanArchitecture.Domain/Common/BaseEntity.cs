d0fb70986d9a6d2038ff85441d517f7e8137696f
saman azadi <samanazadi1996@gmail.com>
2024-09-26 00:13:29 +0330

    'BaseEntity.cs' was modified
    Modified : Src/Core/CleanArchitecture.Domain/Common/BaseEntity.cs

--- Source/Src/Core/CleanArchitecture.Domain/Common/BaseEntity.cs
+++ Source/Src/Core/CleanArchitecture.Domain/Common/BaseEntity.cs
@@ -2,7 +2,7 @@
 {
     public abstract class BaseEntity<TKey>
     {
-        public TKey Id { get; set; }
+        public TKey Id { get; set; } = default!;
     }
     public abstract class BaseEntity : BaseEntity<long>
     {
